e55ae3a47de7a7729b3d004a9c59a040550c1635
robyn <65057146+robynsdev@users.noreply.github.com>
2020-12-03 21:14:09 +1100

    layout and styling done

---
+++ quiz-app/script.js
@@ -0,0 +1,30 @@
+const quizData = [
+  {
+    question: 'In what part of the body would you find the fibula?',
+    a: 'Leg',
+    b: 'Arm',
+    c: 'Head',
+    d: 'Chest'
+    correct: 'a'
+  },{
+    question: 'Hamilton has become one of the biggest musical theatre shows of all time since it debuted in 2015, but what is the name of the acclaimed star who wrote it?'
+    a: 'Min-Manuel Miranda',
+    b: 'Lin-Ranuel Miranda',
+    c: 'Lin-Manuel Miranda',
+    d: 'Lin-Manuel Firanda',
+    correct: 'c'
+  },{
+    question: 'If you have cryophobia, what are you afraid of?'
+    a: 'Spiders',
+    b: 'Lamps',
+    c: 'Enclosed spaces',
+    d: 'Ice/cold'
+    correct: 'd'
+  }
+]
+
+Leg
+Lin-Manuel Miranda
+Ice/cold
+3
+Call of Duty
