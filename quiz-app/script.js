22299fd74fcebf6dc1683d7140638331f748d5f3
robyn <65057146+robynsdev@users.noreply.github.com>
2020-12-03 22:36:17 +1100

    quiz done

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -4,27 +4,87 @@ const quizData = [
     a: 'Leg',
     b: 'Arm',
     c: 'Head',
-    d: 'Chest'
+    d: 'Chest',
     correct: 'a'
   },{
-    question: 'Hamilton has become one of the biggest musical theatre shows of all time since it debuted in 2015, but what is the name of the acclaimed star who wrote it?'
+    question: 'Hamilton has become one of the biggest musical theatre shows of all time since it debuted in 2015, but what is the name of the acclaimed star who wrote it?',
     a: 'Min-Manuel Miranda',
     b: 'Lin-Ranuel Miranda',
     c: 'Lin-Manuel Miranda',
     d: 'Lin-Manuel Firanda',
     correct: 'c'
   },{
-    question: 'If you have cryophobia, what are you afraid of?'
+    question: 'If you have cryophobia, what are you afraid of?',
     a: 'Spiders',
     b: 'Lamps',
     c: 'Enclosed spaces',
-    d: 'Ice/cold'
+    d: 'Ice/cold',
     correct: 'd'
   }
 ]
 
-Leg
-Lin-Manuel Miranda
-Ice/cold
-3
-Call of Duty
+const quiz = document.getElementById("quiz");
+const questionEl = document.getElementById("question")
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+const answerEls = document.querySelectorAll(".answer");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+
+function loadQuiz() {
+  deselectAnswers();
+
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if(answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  
+  if(answer) {
+    if(answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    
+      currentQuiz++;
+      if(currentQuiz < quizData.length) {
+        loadQuiz();
+      } else {
+        quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correct.</h2>
+        <button onclick="location.reload()">Reload</button>
+        `;
+      }
+    }
+});
