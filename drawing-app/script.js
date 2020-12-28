87f645c445bdab0fcd63816450553740f37ea462
robyn <65057146+robynsdev@users.noreply.github.com>
2020-12-28 18:33:31 +1100

    toolbox added

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,11 +1,55 @@
 const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
 const ctx = canvas.getContext("2d");
 
-let size = 30;
+let size = 20;
+let isPressed = false;
+// let x = 50;
+// let y = 50;
 
-canvas.addEventListener("mousedown", () => {});
+canvas.addEventListener("mousedown", () => {
+  isPressed = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+  isPressed = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    drawCircle(x, y);
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, 2 * Math.PI);
+  ctx.fill();
 }
+
+increaseBtn.addEventListener("click", () => {
+  size += 5;
+
+  if (size > 50) {
+    size = 50;
+  }
+});
+decreaseBtn.addEventListener("click", () => {
+  size -= 5;
+
+  if (size < 5) {
+    size = 5;
+  }
+});
+
+// function draw() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   drawCircle(x, y);
+//   requestAnimationFrame(draw);
+// }
+
+// draw();
